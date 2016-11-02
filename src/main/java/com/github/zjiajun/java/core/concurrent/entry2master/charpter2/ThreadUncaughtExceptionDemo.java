eac63ad507c2f1ccf2434bd1f092eb0bf7da82ac
zhujiajun <zhujiajun.me@gmail.com>
2016-11-02 12:44:50 +0800

    uncaughtExceptionHandler

--- src/main/java/com/github/zjiajun/java/core/concurrent/entry2master/charpter2/ThreadUncaughtExceptionDemo.java
+++ src/main/java/com/github/zjiajun/java/core/concurrent/entry2master/charpter2/ThreadUncaughtExceptionDemo.java
@@ -8,6 +8,7 @@ package com.github.zjiajun.java.core.concurrent.entry2master.charpter2;
  */
 public class ThreadUncaughtExceptionDemo implements Runnable {
 
+
     @Override
     public void run() {
 //        java.lang.ArrayIndexOutOfBoundsException: 1
@@ -17,14 +18,25 @@ public class ThreadUncaughtExceptionDemo implements Runnable {
 //        int number = Integer.parseInt("FUCK");
     }
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(new ThreadUncaughtExceptionDemo());
-        thread.setUncaughtExceptionHandler((t, e) -> {
+    private static class ExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
             System.err.println("Exception in thread : " + t.getName());
             System.err.println("Stack trace : ");
             e.printStackTrace(System.err);
             System.err.println("Thread status : " + t.getState());
-        });
+            Thread thread = new Thread(new ThreadUncaughtExceptionDemo());
+            thread.setUncaughtExceptionHandler(new ExceptionHandler());
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        ExceptionHandler exceptionHandler = new ExceptionHandler();
+        ThreadUncaughtExceptionDemo demo = new ThreadUncaughtExceptionDemo();
+        Thread thread = new Thread(demo,"exception-thread");
+        thread.setUncaughtExceptionHandler(exceptionHandler);
         thread.start();
     }
 }
