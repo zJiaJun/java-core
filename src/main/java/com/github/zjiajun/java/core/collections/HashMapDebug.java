89a8dad2d388572cf506ca83e9748f47f630ef12
zhujiajun <zhujiajun.me@gmail.com>
2016-12-07 22:28:52 +0800

    HashMapDebug

--- src/main/java/com/github/zjiajun/java/core/collections/HashMapDebug.java
+++ src/main/java/com/github/zjiajun/java/core/collections/HashMapDebug.java
@@ -9,11 +9,35 @@ import java.util.Map;
  */
 public class HashMapDebug {
 
+    private void put() {
+        Map<HashKey,String> map = new HashMap<>();
+           /*
+         * HashMap 判断key的hashCode，如相等，再判断equals是否相等
+         */
+        HashKey k1 = new HashKey();
+        map.put(k1,"1");
+        HashKey k2 = new HashKey();
+        map.put(k2,"2");
+    }
+
+    private void rehash() {
+        Map<String,String> map = new HashMap<>();
+        for (int i = 1;i <=13;i++) {
+            map.put("key_"+i, "value_"+i);
+            System.out.println(map.size());
+        }
+    }
+
+    private void threshold() {
+        Map<String,String> map = new HashMap<>(16,1.75f);
+        //threshold = 16
+    }
+
     private static class HashKey {
         @Override
         public int hashCode() {
             System.out.println("invoke : hashCode");
-            return 1;
+            return 21;
         }
 
         @Override
@@ -24,13 +48,10 @@ public class HashMapDebug {
     }
 
     public static void main(String[] args) {
-        Map<HashKey,String> map = new HashMap<>();
-        /*
-         * HashMap 判断key的hashCode，如相等，再判断equals是否相等
-         */
-        map.put(new HashKey(),"1");
-        map.put(new HashKey(),"2");
-
+        HashMapDebug hashMapDebug = new HashMapDebug();
+        hashMapDebug.put();
+        hashMapDebug.rehash();
+        hashMapDebug.threshold();
 
     }
 }
