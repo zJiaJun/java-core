c77d4405a868688002cf6bc773e1ed0b8655e69a
zhujiajun <zhujiajun.me@gmail.com>
2017-03-01 21:52:15 +0800

    PipedData

---
+++ src/main/java/com/github/zjiajun/java/core/concurrent/book1/OneByOneRun.java
@@ -0,0 +1,51 @@
+package com.github.zjiajun.java.core.concurrent.book1;
+
+/**
+ * @author zhujiajun
+ * @since 2017/3/1
+ */
+public class OneByOneRun {
+
+    private volatile boolean prevIsA = false;
+
+    public synchronized void backupA() {
+        try {
+            while (prevIsA) {
+                wait();
+            }
+            for (int i = 0; i < 2; i++) {
+                System.out.println("******");
+            }
+            prevIsA = true;
+            notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void backupB() {
+        try {
+            while (!prevIsA) {
+                wait();
+            }
+            for (int i = 0; i < 2; i++) {
+                System.out.println("$$$$$$");
+            }
+            prevIsA = false;
+            notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        OneByOneRun oneByOneRun = new OneByOneRun();
+        for (int i = 0; i < 2; i++) {
+            Thread threadB = new Thread(oneByOneRun::backupB);
+            threadB.start();
+            Thread threadA = new Thread(oneByOneRun::backupA);
+            threadA.start();
+        }
+    }
+}
