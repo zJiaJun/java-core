e13cc1438dc473333c0230469578efc901398f71
zhujiajun <zhujiajun.me@gmail.com>
2017-03-11 22:10:36 +0800

    delete design

--- src/main/java/com/github/zjiajun/java/core/design/decorator/DecoratorSourceable.java
+++
@@ -1,9 +0,0 @@
-package com.github.zjiajun.java.core.design.decorator;
-
-/**
- * Created by zhujiajun
- * 15/2/5 10:35
- */
-public interface DecoratorSourceable {
-    void method();
-}
--- src/main/java/com/github/zjiajun/java/core/design/proxy/ProxySourceable.java
+++
@@ -1,9 +0,0 @@
-package com.github.zjiajun.java.core.design.proxy;
-
-/**
- * Created by zhujiajun
- * 15/2/5 10:41
- */
-public interface ProxySourceable {
-    void method();
-}
