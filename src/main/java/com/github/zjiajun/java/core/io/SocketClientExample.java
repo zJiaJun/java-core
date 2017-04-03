bfbb2b1db034995541d037eb5d3be8367a871611
zhujiajun <zhujiajun.me@gmail.com>
2017-04-03 22:01:22 +0800

    observer

--- src/main/java/com/github/zjiajun/java/core/io/SocketClientExample.java
+++ src/main/java/com/github/zjiajun/java/core/io/SocketClientExample.java
@@ -1,8 +1,6 @@
 package com.github.zjiajun.java.core.io;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
