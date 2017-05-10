83e2a6a1d164c2c313229cde35960d4c3c14f45c
zhujiajun <zhujiajun.me@gmail.com>
2017-05-10 19:48:17 +0800

    modify

--- src/main/java/com/github/zjiajun/java/core/collections/ArrayListDebug.java
+++ src/main/java/com/github/zjiajun/java/core/collections/ArrayListDebug.java
@@ -1,6 +1,7 @@
 package com.github.zjiajun.java.core.collections;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zhujiajun
@@ -9,9 +10,10 @@ import java.util.ArrayList;
 public class ArrayListDebug {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
             list.add(i);
         }
+        list.remove(0);
     }
 }
