26d2a6011903a742fedc8b4af2c4af1398133008
zhujiajun <zhujiajun.me@gmail.com>
2017-09-25 17:45:16 +0800

    future

---
+++ src/main/java/com/github/zjiajun/java/core/concurrent/book3/Interrupted.java
@@ -0,0 +1,45 @@
+package com.github.zjiajun.java.core.concurrent.book3;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhujiajun
+ * @since 2017/9/25
+ */
+public class Interrupted {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread sleepThread = new Thread(() -> {
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("interrupted");
+                    e.printStackTrace();
+                }
+            }
+        }, "sleepThread");
+        sleepThread.setDaemon(true);
+
+        Thread busyThread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+
+            }
+        },"busyThread");
+        busyThread.setDaemon(true);
+
+        sleepThread.start();
+        busyThread.start();
+
+        TimeUnit.SECONDS.sleep(5);
+
+        sleepThread.interrupt();
+        busyThread.interrupt();
+
+
+        System.out.println(sleepThread.getName() + " is " + sleepThread.isInterrupted());
+        System.out.println(busyThread.getName() + " is " + busyThread.isInterrupted());
+
+        TimeUnit.SECONDS.sleep(2);
+    }
+}
