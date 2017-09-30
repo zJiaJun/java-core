76a460a5f5bca75673c5cb10561b91cf23a9a3b6
zhujiajun <zhujiajun.me@gmail.com>
2017-09-30 22:06:57 +0800

    classloader

--- src/main/java/com/github/zjiajun/java/core/classloader/ClassLoaderInheritance.java
+++ src/main/java/com/github/zjiajun/java/core/classloader/ClassLoaderInheritance.java
@@ -2,21 +2,19 @@ package com.github.zjiajun.java.core.classloader;
 
 import com.sun.nio.zipfs.ZipInfo;
 
-import java.net.URL;
-
 /**
  * Created by zhujiajun
  * 16/4/29 22:09
- *
+ * <p>
  * classLoader 层级关系
- *
+ * <p>
  * BootStrapClassLoader  启动类加载器(JAVA_HOME/lib),可以通过-Xbootclasspath选项指定的jar包到内存
  * -Xbootclasspath/a: 把路径添加到已存在BootStrapClassLoader搜索路径的后面
  * -Xbootclasspath/p: 把路径添加到已存在BootStrapClassLoader搜索路径的前面
- *
+ * <p>
  * ExtClassLoader 扩展类加载器(JAVA_HOME/lib/ext),可以通过-Djava.ext.dirs指定位置中的类库加载到内存,如指定了则原有的ext下的java包不会被引入
- *
- *     AppClassLoader 系统类加载器(classpath),可以通过-Djava.class.path,-classpath,-cp
+ * <p>
+ * AppClassLoader 系统类加载器(classpath),可以通过-Djava.class.path,-classpath,-cp
  */
 public class ClassLoaderInheritance {
 
@@ -39,8 +37,10 @@ public class ClassLoaderInheritance {
         System.out.println("ZipInfo.class.getClassLoader() " + classLoader1);
 
 
-        System.out.println(systemClassLoader.getParent());
-        System.out.println(systemClassLoader.getParent().getParent());
+        while (systemClassLoader != null) {
+            System.out.println(systemClassLoader.getClass().getCanonicalName());
+            systemClassLoader = systemClassLoader.getParent();
+        }
 
     }
 }
